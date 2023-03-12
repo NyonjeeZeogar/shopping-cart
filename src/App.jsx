0ed98686a8e203e10ec03ab7091eda32ecce7ab1
Nyonjee Zeogar <54722046+NyonjeeZeogar@users.noreply.github.com>
2023-03-12 18:05:34 -0500

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <div>
+      <div className="body__container">
+        <div className="nav">
+          <Header />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
